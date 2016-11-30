6dc22c8fcf4671a2a3e3dfbd8e4aba5f1d2b2e48
Plieper <liesbeth.slechtvalk@gmail.com>
2016-11-30 15:31:28 +0100

    correcting argument in function

--- creategif.sh
+++ creategif.sh
@@ -1,6 +1,6 @@
 sec="$1"
 nameOfGifFile="$2"
 
-convert -delay sec/1 -loop 10 -layers optimize *.png $nameOfGifFile.gif
+convert -delay $sec -loop 10 -layers optimize *.png $nameOfGifFile.gif
 
 #info: https://www.imagemagick.org/script/convert.php
